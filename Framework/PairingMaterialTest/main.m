9d100a95aa150bb98a640c5dc178460e7406a10f
Jeremy Agostino <jeremyagost@gmail.com>
2014-04-23 18:12:44 -0400

    blah

--- Framework/PairingMaterialTest/main.m
+++ Framework/PairingMaterialTest/main.m
@@ -16,11 +16,14 @@ int main(int argc, const char * argv[])
 	@autoreleasepool {
 	    
 		NSString * pubkeyString = @"-----BEGIN RSA PUBLIC KEY-----\
-		MIGJAoGBAKesA8shuexF81ykLZ3Yr6CXlWorqZOeN/pkYNh7XDQh9f4xMKoQ7dlJ\
-		RV7YTN14k95zIuHj97csCOiUhw3PKinqqy1CMOvDOmQb6Yx8rlYbhd0HIezqP8Xd\
-		WXa6aT9WkyMJXHaqQE7jqJkzO9A2/RczLDMRl0W3gIyVCbyaZ3LfAgMBAAE=\
-		-----END RSA PUBLIC KEY-----";
+MIGJAoGBAKesA8shuexF81ykLZ3Yr6CXlWorqZOeN/pkYNh7XDQh9f4xMKoQ7dlJ\
+RV7YTN14k95zIuHj97csCOiUhw3PKinqqy1CMOvDOmQb6Yx8rlYbhd0HIezqP8Xd\
+WXa6aT9WkyMJXHaqQE7jqJkzO9A2/RczLDMRl0W3gIyVCbyaZ3LfAgMBAAE=\
+-----END RSA PUBLIC KEY-----";
 		NSData * pubkeyData = [pubkeyString dataUsingEncoding:NSUTF8StringEncoding];
+		pubkeyData = [NSData dataWithContentsOfFile:@"/Users/jeremy/Dropbox (Tekserve)/Reversing/DevicePublicKey"];
+		
+		
 		CFMutableDictionaryRef material = SDMMD__CreatePairingMaterial((__bridge CFDataRef)(pubkeyData));
 		CFShow(material);
 	    
